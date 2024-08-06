a82f5e0490a5e71ad8c5a90154497e4328d7dd27
ARNAUDVictor <142483758+ARNAUDVictor@users.noreply.github.com>
2024-08-06 16:47:18 +0200

    Add handler to get file from View to Controller

--- components/fileUpload/fileUploadController.js
+++ components/fileUpload/fileUploadController.js
@@ -1,9 +1,14 @@
 export class FileUploadController{
     constructor(view){
         this.view = view;
+        this.view.bindFileUpload(this.handleFileUpload)
     }
 
     getFileInput(){
         return this.view.fileInput;
+    }
+
+    handleFileUpload(file){
+        console.log(file);
     }
 }
--- components/fileUpload/fileUploadView.js
+++ components/fileUpload/fileUploadView.js
@@ -9,4 +9,36 @@ export class FileUploadView{
 
         return element;
     }
+
+    bindFileUpload(handler) {
+        this.fileInput.addEventListener("change", event => {
+
+            console.log(this.fileInput.files);
+            handler(this.fileInput.files)
+        })
+    }
+
 }
+
+
+/*
+ca marche :
+
+addListener(handler) {
+        console.log(this.fileInput);
+        this.fileInput.addEventListener("change", event => {
+
+            console.log(this.fileInput.files);
+        })
+    }
+
+ca marche pas
+addListener(handler) {
+        console.log(this.fileInput);
+        this.fileInput.addEventListener("change", function(event) {
+
+            console.log(this.fileInput.files);
+        })
+    }
+
+*/
